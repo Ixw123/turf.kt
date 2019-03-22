ea50b19e814db9e4888df85003942ac6268e5bf1
Ixw123 <micah.church.a@gmail.com>
2019-03-21 21:32:01 -0400

    still trying to get it to work

--- src/main/kotlin/org/turfkt/Measurement.kt
+++ src/main/kotlin/org/turfkt/Measurement.kt
@@ -1,17 +1,50 @@
 package org.turfkt
 
+import geojsonkt.Geometry
+import geojsonkt.Feature
+import geojsonkt.FeatureCollection
+import geojsonkt.Polygon
+import geojsonkt.MultiPolygon
+import geojsonkt.Point
+import geojsonkt.MultiPoint
+import geojsonkt.LineString
+import geojsonkt.MultiLineString
+//import geojsonkt.Position
+
 import kotlin.math.sin
+import kotlin.math.abs
 
-// NOTE: Change RADIUS => earthRadius
-const RADIUS = 6378137
-
-fun getPolygonArea(out T): Double {
+private fun calculateArea( geom: Geometry): Number{
     var total = 0.0
-    if (T && T.size > 0) {
-
+    var i: Int
+    when (geom) {
+        is Polygon -> polygonArea(geom.coordinates)
+        is MultiPolygon -> {
+            for (i in 0..(geom.coordinates.map { it[0] }.size - 1)) {
+                total += polygonArea(geom.get(i).coordinates)
+            }
+            return total
+        }
+        is Point -> return 0
+        is MultiPoint -> return 0 
+        is LineString -> return 0
+        is MultiLineString -> return 0
+        else -> throw UnsupportedOperationException("Can not calculate area of unrecognized Geometry type: ${geom::class.java.name}")
     }
+    return 0
 }
-fun ringArea(out T): Double {
+
+fun polygonArea(coordinates: Any): Double {
+    var total = 0.0
+    if (coordinates && coordinates.size > 0) {
+        total += abs(ringArea(coordinates[0]))
+        for (i: Int in 1..(coordinates[0][0].size - 1)){
+            total -= abs(ringArea(coordinates[i]))
+        }
+    }
+    return total
+}
+fun ringArea(val coords: Array<Number>): Double {
     var p1: Double
     var p2: Double
     var p3: Double
@@ -20,7 +53,7 @@ fun ringArea(out T): Double {
     var upperIndex: Int
     var i: Int
     var total = 0.0
-    const coordinateLength = T.size
+    const coordinateLength = coords.size
 
     if (coordinateLength > 2) {
         for (i in 0..(coordinateLength - 1)) {
@@ -44,14 +77,18 @@ fun ringArea(out T): Double {
             p3 = T[upperIndex]
             total += (p3[0].toRadians() - p1[0].toRadians()) * sin(p2[1].toRadians())
         }   
-        total *= (RADIUS * RADIUS / 2)
+        total *= (EARTH_RADIUS * EARTH_RADIUS / 2)
     } 
     return total
 }
+//private fun area()
+fun Geometry.area(geojson: Any) { return geojson.fold(geojson) { return value + calculateArea(geom) }
+fun Feature.area(geojson: Any) = Geometry.area(geojson)
 
-fun Feature.area(): Double {
-} 
-fun FeatureCollections.area(): Double {
+fun FeatureCollection.area(geojson: Any): Double {
     var total = 0.0
-
+    for (feature in geojson) {
+        total += Feature.area(feature)
+    }
+    return total
 }
