897ee5f5f76dd896fd01597df06279b315410bf5
Ixw123 <micah.church.a@gmail.com>
2019-03-27 16:31:26 -0400

    Applied suggested changes of the PR

--- src/main/kotlin/org/turfkt/Measurement.kt
+++ src/main/kotlin/org/turfkt/Measurement.kt
@@ -14,20 +14,14 @@ import geojsonkt.Position
 import kotlin.math.sin
 import kotlin.math.abs
 
-// For testing purposes
-//val EARTHRADIUS = 6378137
-
 private fun calculateArea( geom: Geometry): Double{
     var total = 0.0
-    //var i: Int
     when (geom) {
         is Polygon -> { 
-            //val size = geom.coordinates.size
             return polygonArea(geom.coordinates)
         }
         is MultiPolygon -> {
-            //val size = geom.coordinates.size
-            for (i in 0..(geom.coordinates.size - 1)) {
+            for (i in 0 until geom.coordinates.size) {
                 total += polygonArea(geom.coordinates[i])
             }
             return total
@@ -42,15 +36,15 @@ private fun calculateArea( geom: Geometry): Double{
 
 private fun polygonArea(coordinates: Array<Array<Position>>): Double {
     var total = 0.0
-    //val coordSize = coordinates.size
     if (coordinates.isNotEmpty()) {
         total += abs(ringArea(coordinates[0]))
-        for (i: Int in 1..(coordinates.size - 1)){
+        for (i in 1 until coordinates.size){
             total -= abs(ringArea(coordinates[i]))
         }
     }
     return total
 }
+
 private fun ringArea(coords: Array<Position>): Double {
     var p1: Position
     var p2: Position
@@ -58,13 +52,11 @@ private fun ringArea(coords: Array<Position>): Double {
     var lowerIndex: Int
     var middleIndex: Int
     var upperIndex: Int
-    //var i: Int
     var total = 0.0
 
     val coordinateLength = coords.size
-
     if (coordinateLength > 2) {
-        for (i in 0..(coordinateLength - 1)) {
+        for (i in 0 until coordinateLength) {
             if (i == coordinateLength - 2) { // i = N - 2
                 lowerIndex = coordinateLength - 2
                 middleIndex = coordinateLength - 1
@@ -89,10 +81,7 @@ private fun ringArea(coords: Array<Position>): Double {
     } 
     return total
 }
-//private fun area()
-fun Geometry.area() = calculateArea(this)
-fun Polygon.area() = calculateArea(this)
-fun MultiPolygon.area() = calculateArea(this)
+fun Geometry.area(): Double = calculateArea(this)
 fun Feature<*>.area(): Double {
     return when (geometry) {
         is Polygon -> geometry.area()
@@ -112,16 +101,3 @@ fun FeatureCollection.area(): Double {
     }
     return total
 }
-
-//fun main() {
-//    val polygon1: Array<Array<Position>>
-//    var position1 = Position(doubleArrayOf(125.0, -15.0))
-//    var position2 = Position(doubleArrayOf(113.0, -22.0))
-//    var position3 = Position(doubleArrayOf(154.0, -27.0))
-//    var position4 = Position(doubleArrayOf(144.0, -15.0))
-//    var position5 = Position(doubleArrayOf(125.0, -15.0))
-//    polygon1 = arrayOf(arrayOf(position1, position2, position3, position4, position5))
-//    val poly1 = Polygon(polygon1)
-//    val area1 = poly1.area()
-//    println("poly1 area: $area1")
-//}
