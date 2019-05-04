8595980a047b2b5886b8bbdf8b1ef329da0aeb93
Micah <churchm@sunyit.edu>
2019-05-04 02:00:34 -0400

    Revert "Not yet compiling on either of these, working on BBox currently"
    This reverts commit b4938a5f7ced9b86d2178e8211084619e14c11f8.

--- src/main/kotlin/turfkt/Center.kt
+++
@@ -1,9 +0,0 @@
-fun center<P = Properties>(
-    geojson: AllGeoJSON,
-    options: {properties?: P, bbox?: BBox, id?: Id } = {}
-): Feature<Point> {
-    val ext = bbox(geojson);
-    val x = (ext[0] + ext[2]) / 2;
-    val y = (ext[1] + ext[3]) / 2;
-    return point([x, y], options.properties, options);
-}
