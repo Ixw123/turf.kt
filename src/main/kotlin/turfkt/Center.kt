b4938a5f7ced9b86d2178e8211084619e14c11f8
Micah <churchm@sunyit.edu>
2019-05-04 02:00:34 -0400

    Not yet compiling on either of these, working on BBox currently

---
+++ src/main/kotlin/turfkt/Center.kt
@@ -0,0 +1,9 @@
+fun center<P = Properties>(
+    geojson: AllGeoJSON,
+    options: {properties?: P, bbox?: BBox, id?: Id } = {}
+): Feature<Point> {
+    val ext = bbox(geojson);
+    val x = (ext[0] + ext[2]) / 2;
+    val y = (ext[1] + ext[3]) / 2;
+    return point([x, y], options.properties, options);
+}
